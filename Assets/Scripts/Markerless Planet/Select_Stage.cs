8f9ce12e17d7bf6c33c169bc77c10c72974ab203
Reiyx <45916191+ReiyxKee@users.noreply.github.com>
2022-05-13 04:24:47 +0800

    Tutorial Guide 2
    - Tutorial Update 90%, require some final touch up
    - Path Mesh Updated with UV
    - Path Mesh Texture
    -Single Run Tutorial

--- Assets/Scripts/Markerless Planet/Select_Stage.cs
+++ Assets/Scripts/Markerless Planet/Select_Stage.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine.Events;
 using UnityEngine.EventSystems;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.XR.ARFoundation;
 using TMPro;
 
@@ -56,7 +57,7 @@ public class Select_Stage : MonoBehaviour
     public float t_un;
     public GameObject t_Start;
 
-
+    public Button ResetAR;
     // Start is called before the first frame update
     void Start()
     {
@@ -71,10 +72,13 @@ public class Select_Stage : MonoBehaviour
         UI_DescPrev.SetActive(true);
         if (PlayerPrefs.HasKey("Tutorial"))
         {
+            Debug.Log("First Tutorial Found");
             Tutorial_On = PlayerPrefs.GetInt("Tutorial") == 1 ? true : false;
         }
         else
         {
+            Debug.Log("First Tutorial Not Found");
+            PlayerPrefs.SetInt("Tutorial", 1);
             Tutorial_On = true;
         }
     }
@@ -119,8 +123,7 @@ public class Select_Stage : MonoBehaviour
         }
 
         if (GameParent == null)
-        {
-            
+        {            
             if (GameObject.Find("Game_SolarSys"))
             {
                 GameParent = GameObject.Find("Game_SolarSys/Sun/Childrens").gameObject;
@@ -153,90 +156,7 @@ public class Select_Stage : MonoBehaviour
         {
             if (Tutorial_On)
             {
-                //Select Earth
-                if (tutorial.Guide_TapWorld_Done == false)
-                {
-                    tutorial.Target = Planets[3].transform.GetChild(0).gameObject;
-                    tutorial.Guide_TapWorld = true;
-                }
-
-                if (!tutorial.Guide_TapWorld_Done && tutorial.Guide_TapWorld && CurrentPlanet_Code == 3)
-                {
-                    tutorial.Guide_TapWorld_Done = true;
-                    tutorial.Guide_TapWorld = true;
-                    tutorial.Target = null;                   
-                }
-
-
-                //About
-                if (tutorial.Guide_TapWorld_Done && !tutorial.Guide_TapAboutWorld)
-                {
-                    tutorial.Target = t_AboutStage;
-                    tutorial.Guide_TapAboutWorld = true;
-                }
-
-                if (!tutorial.Guide_TapAboutWorld_Done &&tutorial.Guide_TapAboutWorld && !t_AboutStage.gameObject.activeInHierarchy)
-                {
-                    tutorial.Guide_TapAboutWorld_Done = true;
-                    tutorial.Guide_TapAboutWorld = true;
-                    tutorial.Target = null;
-                }
-
-                //Enter
-                if (tutorial.Guide_TapAboutWorld_Done && !tutorial.Guide_TapEnterWorld)
-                {
-                    tutorial.Target = t_EnterStage;
-                    tutorial.Guide_TapEnterWorld = true;
-                }
-
-                if (!tutorial.Guide_TapEnterWorld_Done && tutorial.Guide_TapEnterWorld && !t_EnterStage.gameObject.activeInHierarchy)
-                {
-                    tutorial.Guide_TapEnterWorld_Done = true;
-                    tutorial.Guide_TapEnterWorld = true;
-                    tutorial.Target = null;
-                }
-
-                //Explain Unlockable
-                if (tutorial.Guide_TapEnterWorld_Done && !tutorial.Guide_Unlockable)
-                {
-                    tutorial.BoxTarget = t_Unlockable;
-                    tutorial.Guide_Unlockable = true;
-                    OnTap = false;
-                }
-
-                if (tutorial.Guide_Unlockable && !tutorial.Guide_Unlockable_Done)
-                {
-                    if (Input.touchCount == 1 && !OnTap)
-                    {
-                        OnTap = true;
-                    }
-                    else if(Input.touchCount == 0)
-                    {
-                        OnTap = false;
-                    }
-
-                    t_un += Time.deltaTime;
-                }
-
-                if (!tutorial.Guide_Unlockable_Done && tutorial.Guide_Unlockable && t_un > 1.0f && OnTap)
-                {
-                    tutorial.Guide_Unlockable_Done = true;
-                    tutorial.Guide_Unlockable = true;
-                    tutorial.BoxTarget = null;
-                }
-
-                if (tutorial.Guide_Unlockable_Done && !tutorial.Guide_StartGame)
-                {
-                    tutorial.Target = t_Start;
-                    tutorial.Guide_StartGame = true;
-                }
-
-                if (!tutorial.Guide_StartGame_Done && tutorial.Guide_StartGame && !t_Start.gameObject.activeInHierarchy)
-                {
-                    tutorial.Guide_StartGame_Done = true;
-                    tutorial.Guide_StartGame = true;
-                    tutorial.Target = null;
-                }
+                Tutorial_Manage();
             }
 
             if (CurrentPlanet != null)
@@ -483,5 +403,323 @@ public class Select_Stage : MonoBehaviour
         //GameObject.Find("PlanetGame").gameObject.GetComponent<Animator>().SetBool("Arrive", true);
     }
 
+    public void ToggleARReset()
+    {
+        ResetAR.enabled = !ResetAR.enabled;
+    }
 
+    public void Tutorial_Manage()
+    {
+        //Select Earth
+        if (tutorial.Guide_TapWorld_Done == false)
+        {
+            tutorial.Target = Planets[3].transform.GetChild(0).gameObject;
+            tutorial.Guide_TapWorld = true;
+        }
+
+        if (!tutorial.Guide_TapWorld_Done && tutorial.Guide_TapWorld && CurrentPlanet_Code == 3)
+        {
+            tutorial.Guide_TapWorld_Done = true;
+            tutorial.Guide_TapWorld = true;
+            tutorial.Target = null;
+        }
+
+
+        //About
+        if (tutorial.Guide_TapWorld_Done && !tutorial.Guide_TapAboutWorld)
+        {
+            tutorial.Target = t_AboutStage;
+            tutorial.Guide_TapAboutWorld = true;
+        }
+
+        if (!tutorial.Guide_TapAboutWorld_Done && tutorial.Guide_TapAboutWorld && !t_AboutStage.gameObject.activeInHierarchy)
+        {
+            tutorial.Guide_TapAboutWorld_Done = true;
+            tutorial.Guide_TapAboutWorld = true;
+            tutorial.Target = null;
+        }
+
+        //Enter
+        if (tutorial.Guide_TapAboutWorld_Done && !tutorial.Guide_TapEnterWorld)
+        {
+            tutorial.Target = t_EnterStage;
+            tutorial.Guide_TapEnterWorld = true;
+        }
+
+        if (!tutorial.Guide_TapEnterWorld_Done && tutorial.Guide_TapEnterWorld && !t_EnterStage.gameObject.activeInHierarchy)
+        {
+            tutorial.Guide_TapEnterWorld_Done = true;
+            tutorial.Guide_TapEnterWorld = true;
+            tutorial.Target = null;
+        }
+
+        //Explain Unlockable
+        if (tutorial.Guide_TapEnterWorld_Done && !tutorial.Guide_Unlockable)
+        {
+            tutorial.BoxTarget = t_Unlockable;
+            tutorial.Guide_Unlockable = true;
+            OnTap = false;
+        }
+
+        if (tutorial.Guide_Unlockable && !tutorial.Guide_Unlockable_Done)
+        {
+            if (Input.touchCount == 1 && !OnTap)
+            {
+                OnTap = true;
+            }
+            else if (Input.touchCount == 0)
+            {
+                OnTap = false;
+            }
+
+            t_un += Time.deltaTime;
+        }
+
+        if (!tutorial.Guide_Unlockable_Done && tutorial.Guide_Unlockable && t_un > 1.0f && OnTap)
+        {
+            tutorial.Guide_Unlockable_Done = true;
+            tutorial.Guide_Unlockable = true;
+            tutorial.BoxTarget = null;
+        }
+
+        if (tutorial.Guide_Unlockable_Done && !tutorial.Guide_StartGame)
+        {
+            tutorial.Target = t_Start;
+            tutorial.Guide_StartGame = true;
+        }
+
+        if (!tutorial.Guide_StartGame_Done && tutorial.Guide_StartGame && !t_Start.gameObject.activeInHierarchy)
+        {
+            tutorial.Guide_StartGame_Done = true;
+            tutorial.Guide_StartGame = true;
+            tutorial.Target = null;
+        }
+
+        //Guide start 
+        if (tutorial.Guide_StartGame_Done && !tutorial.Tutorial_Start)
+        {
+            OnTap = false;
+            t_un = 0;
+            tutorial.Tutorial_Start = true;
+            tutorial.CircleTarget = GameObject.Find("PlayerAxis/Player");
+        }
+
+        if (tutorial.Tutorial_Start)
+        {
+            if (Input.touchCount == 1 && !OnTap)
+            {
+                OnTap = true;
+            }
+            else if (Input.touchCount == 0)
+            {
+                OnTap = false;
+            }
+
+            t_un += Time.deltaTime;
+        }
+
+        //Guide fuel
+        if (!tutorial.Tutorial_Start_Done && tutorial.Tutorial_Start && t_un > 2.0f && OnTap)
+        {
+            tutorial.CircleTarget = null;
+            tutorial.Target = null;
+            OnTap = false;
+            t_un = 0;
+            tutorial.Tutorial_Start_Done = true;
+            tutorial.Tutorial_Fuel = true;
+        }
+
+
+        if (tutorial.Tutorial_Fuel)
+        {
+            if (Input.touchCount == 1 && !OnTap)
+            {
+                OnTap = true;
+            }
+            else if (Input.touchCount == 0)
+            {
+                OnTap = false;
+            }
+
+            t_un += Time.deltaTime;
+        }
+
+        //Guide Move 1
+        if (!tutorial.Tutorial_Fuel_Done && tutorial.Tutorial_Fuel && t_un > 2.0f && OnTap)
+        {
+            OnTap = false;
+            t_un = 0;
+            tutorial.Tutorial_Fuel_Done = true;
+            tutorial.Tutorial_Move_1 = true;
+        }
+
+
+        if (tutorial.Tutorial_Move_1)
+        {
+            if (Input.touchCount == 1 && !OnTap)
+            {
+                OnTap = true;
+            }
+            else if (Input.touchCount == 0)
+            {
+                OnTap = false;
+            }
+
+            t_un += Time.deltaTime;
+        }
+
+        //Guide Move 2
+        if (!tutorial.Tutorial_Move_1_Done && tutorial.Tutorial_Move_1 && t_un > 2.0f && OnTap)
+        {
+            OnTap = false;
+            t_un = 0;
+            tutorial.Tutorial_Move_1_Done = true;
+            tutorial.Tutorial_Move_2 = true;
+        }
+
+
+        if (tutorial.Tutorial_Move_2)
+        {
+            if (Input.touchCount == 1 && !OnTap)
+            {
+                OnTap = true;
+            }
+            else if (Input.touchCount == 0)
+            {
+                OnTap = false;
+            }
+
+            t_un += Time.deltaTime;
+        }
+
+        //Guide Move 3
+        if (!tutorial.Tutorial_Move_2_Done && tutorial.Tutorial_Move_2 && t_un > 2.0f && OnTap)
+        {
+            OnTap = false;
+            t_un = 0;
+            tutorial.Tutorial_Move_2_Done = true;
+            tutorial.Tutorial_Move_3 = true;
+        }
+
+
+        if (tutorial.Tutorial_Move_3)
+        {
+            if (Input.touchCount == 1 && !OnTap)
+            {
+                OnTap = true;
+            }
+            else if (Input.touchCount == 0)
+            {
+                OnTap = false;
+            }
+
+            t_un += Time.deltaTime;
+        }
+
+        //Guide Move 4
+        if (!tutorial.Tutorial_Move_3_Done && tutorial.Tutorial_Move_3 && t_un > 2.0f && OnTap)
+        {
+            OnTap = false;
+            t_un = 0;
+            tutorial.Tutorial_Move_3_Done = true;
+            tutorial.Tutorial_Move_4 = true;
+        }
+
+
+        if (tutorial.Tutorial_Move_4)
+        {
+            if (Input.touchCount == 1 && !OnTap)
+            {
+                OnTap = true;
+            }
+            else if (Input.touchCount == 0)
+            {
+                OnTap = false;
+            }
+
+            t_un += Time.deltaTime;
+        }
+
+        //Guide Move 5
+        if (!tutorial.Tutorial_Move_4_Done && tutorial.Tutorial_Move_4 && t_un > 2.0f && OnTap)
+        {
+            OnTap = false;
+            t_un = 0;
+            tutorial.Tutorial_Move_4_Done = true;
+            tutorial.Tutorial_Move_5 = true;
+        }
+
+
+        if (tutorial.Tutorial_Move_5)
+        {
+            if (Input.touchCount == 1 && !OnTap)
+            {
+                OnTap = true;
+            }
+            else if (Input.touchCount == 0)
+            {
+                OnTap = false;
+            }
+
+            t_un += Time.deltaTime;
+        }
+
+        //Guide Move 6
+        if (!tutorial.Tutorial_Move_5_Done && tutorial.Tutorial_Move_5 && t_un > 2.0f && OnTap)
+        {
+            OnTap = false;
+            t_un = 0;
+            tutorial.Tutorial_Move_5_Done = true;
+            tutorial.Tutorial_Move_6 = true;
+        }
+
+
+        if (tutorial.Tutorial_Move_6)
+        {
+            if (Input.touchCount == 1 && !OnTap)
+            {
+                OnTap = true;
+            }
+            else if (Input.touchCount == 0)
+            {
+                OnTap = false;
+            }
+
+            t_un += Time.deltaTime;
+        }
+
+        //Guide Move 7
+        if (!tutorial.Tutorial_Move_6_Done && tutorial.Tutorial_Move_6 && t_un > 2.0f && OnTap)
+        {
+            OnTap = false;
+            t_un = 0;
+            tutorial.Tutorial_Move_6_Done = true;
+            tutorial.Tutorial_Move_7 = true;
+        }
+
+
+        if (tutorial.Tutorial_Move_7)
+        {
+            if (Input.touchCount == 1 && !OnTap)
+            {
+                OnTap = true;
+            }
+            else if (Input.touchCount == 0)
+            {
+                OnTap = false;
+            }
+
+            t_un += Time.deltaTime;
+        }
+
+
+        if (!tutorial.Tutorial_Move_7_Done && tutorial.Tutorial_Move_7 && t_un > 2.0f && OnTap)
+        {
+            OnTap = false;
+            t_un = 0;
+            tutorial.Tutorial_Move_7_Done = true;
+            PlayerPrefs.SetInt("Tutorial", 0);
+        }
+
+    }
 }
--- Assets/Scripts/Tutorial_Script.cs
+++ Assets/Scripts/Tutorial_Script.cs
@@ -2,11 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
+
 public class Tutorial_Script : MonoBehaviour
 {
     public GameObject Guide;
     public GameObject BoxGuide;
+    public GameObject CircleGuide;
     public Animate_2DSprite anim;
+
+    public TextMeshProUGUI Instruction;
+    public TextMeshProUGUI Instruction_2;
+    public TextMeshProUGUI TapAndContinue;
 
     public Sprite[] SwipeUp;
     public Sprite[] SwipeDown;
@@ -17,26 +24,64 @@ public class Tutorial_Script : MonoBehaviour
     public Camera cam;
     public GameObject Target;
     public GameObject BoxTarget;
+    public GameObject CircleTarget;
 
     public bool Guide_TapWorld;
     public bool Guide_TapWorld_Done;
+    public GameObject Blocker_TapWorld;
 
     public bool Guide_TapAboutWorld;
     public bool Guide_TapAboutWorld_Done;
+    public GameObject Blocker_TapAboutWorld;
 
     public bool Guide_TapEnterWorld;
     public bool Guide_TapEnterWorld_Done;
+    public GameObject Blocker_TapEnterWorld;
 
     public bool Guide_Unlockable;
     public bool Guide_Unlockable_Done;
+    public GameObject Blocker_Guide_Unlockable;
 
     public bool Guide_StartGame;
     public bool Guide_StartGame_Done;
+    public GameObject Blocker_Guide_StartGame;
+
+    public GameObject Blocker_Tutorial_Full;
+
+    public bool Tutorial_Start;
+    public bool Tutorial_Start_Done;
+    
+    public bool Tutorial_Fuel;
+    public bool Tutorial_Fuel_Done;
+    public GameObject Blocker_Tutorial_Fuel;
+    public GameObject FuelGuide;
+
+    public bool Tutorial_Move_1;
+    public bool Tutorial_Move_1_Done;
+
+    public bool Tutorial_Move_2;
+    public bool Tutorial_Move_2_Done;
+
+    public bool Tutorial_Move_3;
+    public bool Tutorial_Move_3_Done;
+
+    public bool Tutorial_Move_4;
+    public bool Tutorial_Move_4_Done;
+
+    public bool Tutorial_Move_5;
+    public bool Tutorial_Move_5_Done;
+
+    public bool Tutorial_Move_6;
+    public bool Tutorial_Move_6_Done;
+
+    public bool Tutorial_Move_7;
+    public bool Tutorial_Move_7_Done;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        Blocker_TapAboutWorld.SetActive(false);
+        FuelGuide.SetActive(false);
     }
 
     // Update is called once per frame
@@ -60,9 +105,20 @@ public class Tutorial_Script : MonoBehaviour
             Guide.SetActive(true);
         }
 
+        if (CircleTarget == null)
+        {
+            CircleGuide.SetActive(false);
+        }
+        else
+        {
+            CircleGuide.SetActive(true);
+        }
+
         if (Guide_TapWorld && !Guide_TapWorld_Done)
         {
             //Select Planet
+            Blocker_TapWorld.SetActive(true);
+            Instruction.text = "WELCOME TO SOLIBERATE";
             anim.Frames = Tap;
             Guide.GetComponent<RectTransform>().position = cam.WorldToScreenPoint(Target.gameObject.transform.position);
         }
@@ -70,28 +126,120 @@ public class Tutorial_Script : MonoBehaviour
         if (Guide_TapAboutWorld && !Guide_TapAboutWorld_Done)
         {
             //About Planet
+            Blocker_TapWorld.SetActive(false);
+            Blocker_TapAboutWorld.SetActive(true);
+            Instruction.text = "TAP HERE TO LOOK AT THE DETIALS ABOUT THE PLANET";
             anim.Frames = Tap;
             Guide.GetComponent<RectTransform>().position = Target.GetComponent<RectTransform>().position;
         }
 
         if (Guide_TapEnterWorld && !Guide_TapEnterWorld_Done)
         {
-            //About Planet
+            //View Planet
+            Blocker_TapAboutWorld.SetActive(false);
+            Blocker_TapEnterWorld.SetActive(true);
+            Instruction.text = "TAP HERE TO HAVE A BETTER VIEW OF THE PLANET";
             anim.Frames = Tap;
             Guide.GetComponent<RectTransform>().position = Target.GetComponent<RectTransform>().position;
         }
 
         if (Guide_Unlockable && !Guide_Unlockable_Done)
         {
-            //About Planet
+            //Unlockable Planet
+            Blocker_TapEnterWorld.SetActive(false);
+            Blocker_Guide_Unlockable.SetActive(true);
+            Instruction.text = "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\nTHESE ARE SOME UNLOCKABLE TRIVIAL ABOUT THE PLANET.\n\nYOU CAN UNLOCK THEM WHILE TRAVELING ON THE PLANET";
+            TapAndContinue.text = "-TAP AND CONTINUE-";
             BoxGuide.GetComponent<RectTransform>().position = BoxTarget.GetComponent<RectTransform>().position;
+
         }
 
         if (Guide_StartGame && !Guide_StartGame_Done)
         {
-            //About Planet
+            //Start Planet
+            Blocker_Guide_Unlockable.SetActive(false);
+            Blocker_Guide_StartGame.SetActive(true);
+
+            Instruction.text = "LET'S START THE MISSION";
+            TapAndContinue.text = "";
             anim.Frames = Tap;
             Guide.GetComponent<RectTransform>().position = Target.GetComponent<RectTransform>().position;
+        }
+
+        if (Tutorial_Start && !Tutorial_Start_Done)
+        {
+            Instruction.text = "";
+            Instruction_2.text = "IN THIS MISSION WE WILL TRAVEL THE EARTH WITH THE SPACECRAFT";
+            Blocker_Guide_StartGame.SetActive(false);
+            Blocker_Tutorial_Full.SetActive(true);
+
+            CircleGuide.GetComponent<RectTransform>().position = cam.WorldToScreenPoint(CircleTarget.gameObject.transform.position);
+            TapAndContinue.text = "-TAP AND CONTINUE-";
+        }
+
+        if (!Tutorial_Fuel_Done && Tutorial_Fuel)
+        {
+            //FUEL
+            Blocker_Tutorial_Full.SetActive(true);
+            Instruction_2.text = "THE SPACECRAFT HAVE LIMITED FUEL\n\nSO PLAN YOUR TRAVEL BEFORE MOVING";
+            FuelGuide.SetActive(true);
+            TapAndContinue.text = "-TAP AND CONTINUE-";
+        }
+
+        if (!Tutorial_Move_1_Done && Tutorial_Move_1)
+        {
+            //SWIPE
+            FuelGuide.SetActive(false);
+            Instruction_2.text = "YOU CAN MOVE THE SPACECRAFT BY SWIPING THE SCREEN";
+            TapAndContinue.text = "-TAP AND CONTINUE-";
+        }
+
+        if (!Tutorial_Move_2_Done && Tutorial_Move_2)
+        {
+            //SWIPE
+            Instruction_2.text = "THE SPACECRAFT WILL MOVE BASE ON THE DIRECTION YOU FACING IT";
+            TapAndContinue.text = "-TAP AND CONTINUE-";
+        }
+
+        if (!Tutorial_Move_3_Done && Tutorial_Move_3)
+        {
+            //SWIPE
+            Instruction_2.text = "YOU CAN LOOK AT THE INDICATOR TO KNOW WHICH DIRECTION IT WILL MOVE TO WHEN SWIPING";
+            TapAndContinue.text = "-TAP AND CONTINUE-";
+        }
+
+        if (!Tutorial_Move_4_Done && Tutorial_Move_4)
+        {
+            //SWIPE
+            Instruction_2.text = "PASSING THE SPACE STATION WILL SWITCH THEM BETWEEN RED AND GREEN";
+            TapAndContinue.text = "-TAP AND CONTINUE-";
+        }
+
+        if (!Tutorial_Move_5_Done && Tutorial_Move_5)
+        {
+            //SWIPE
+            Instruction_2.text = "TURNING ALL SPACE STATION GREEN WILL ACTIVATE THE DYSON SCANNER";
+            TapAndContinue.text = "-TAP AND CONTINUE-";
+        }
+
+        if (!Tutorial_Move_6_Done && Tutorial_Move_6)
+        {
+            //SWIPE
+            Instruction_2.text = "THIS WILL HELP US TO UNDERSTAND MORE ABOUT THE PLANET WE TRAVELLED";
+            TapAndContinue.text = "-TAP AND CONTINUE-";
+        }
+
+        if (!Tutorial_Move_7_Done && Tutorial_Move_7)
+        {
+            //SWIPE
+            Instruction_2.text = "GOOD LUCK AND GODSPEED";
+            TapAndContinue.text = "-TAP AND CONTINUE-";
+        }
+
+        if (Tutorial_Move_7_Done)
+        {
+            Instruction_2.text = "";
+            TapAndContinue.text = "";
         }
     }
 }
